8c560963ffc8a28a90974b52ce3bdbba17d090b4
Slava Morgachev <mwm1955@gmail.com>
2011-03-29 16:55:48 +0300

    Before upgrate to rails 3.0.5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,17 @@
-set :application, "utilities"
-set :repository,  "git://github.com/slavam/utilities.git"
+require 'bundler/capistrano'
+set :application, "scc"
 set :deploy_to, "/var/www/#{application}"
-set :user, '.....'
-set :scm_username, "slavam"
+set :user, 'root'
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
+set :scm_username, "slavam"
+set :repository,  "git://github.com/slavam/utilities.git"
 
-role :web, "10.40.28..."                          # Your HTTP server, Apache/etc
-role :app, "10.40.28..."                          # This may be the same as your `Web` server
-role :db,  "10.40.28.35", :primary => true # This is where Rails migrations will run
+role :web, "10.40.28.10"                          # Your HTTP server, Apache/etc
+role :app, "10.40.28.10"                          # This may be the same as your `Web` server
+role :db,  "10.40.28.10", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 

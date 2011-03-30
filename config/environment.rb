d67329342276de37d06cd310141e3f86a6ee6d9b
Slava Morgachev <mwm1955@gmail.com>
2011-03-30 10:13:50 +0300

    After upgrate rails to 3.0.5

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Utilities::Application.initialize!
-
-require 'openssl'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Utilities::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-#  config.autoload_paths << "#{::Rails.root.to_s}/vendor/prawn/lib"
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
